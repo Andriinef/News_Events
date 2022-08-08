69cfa4022e7463bcb2d9efb0e23c853c71dff7e8
Andriinef <andrii.nefedov@nure.ua>
2022-08-08 18:38:19 +0300

    lesson 11

--- news/events/models.py
+++ news/events/models.py
@@ -1,3 +1,18 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+class NewsEvents(models.Model):
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(blank=True, verbose_name="Текст статті")
+    created_at = models.DateTimeField(auto_now=True, verbose_name="Час створення")
+    updated_at =models.DateTimeField(auto_now_add=True, verbose_name="Час зміни")
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name="Фото")
+    is_published = models.BooleanField(default=True, verbose_name="Публікація")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категорії")
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категорія")
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
--- news/events/views.py
+++ news/events/views.py
@@ -1,3 +1,14 @@
+from multiprocessing import context
+from urllib import request
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from .models import *
 
 # Create your views here.
+def index(request):
+  news = NewsEvents.objects.order_by('-created_at')
+  context = {'news': news,
+            'titel': 'Сисок новостей'
+  }
+  return render(request, 'events/index.html', context)
