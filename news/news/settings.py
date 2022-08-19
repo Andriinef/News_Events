efde4971f2571b906d09e2d98befa6c125b9c9db
Andriinef <andrii.nefedov@nure.ua>
2022-08-19 16:35:35 +0300

    finizs_DEBUG = False

--- news/news/settings.py
+++ news/news/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^(1r&-i_$ggm*ok0kmy&+g-ye(li$jr1x9cnsamef$$gl(#5eu"
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
