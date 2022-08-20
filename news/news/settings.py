39250fd49260652f5a5ddcba2a509a402838ac7a
Andriinef <andrii.nefedov@nure.ua>
2022-08-20 14:33:13 +0300

    Heroku_1

--- news/news/settings.py
+++ news/news/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^(1r&-i_$ggm*ok0kmy&+g-ye(li$jr1x9cnsamef$$gl(#5eu
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", '.herokuapp.com']
 
 
 # Application definition
