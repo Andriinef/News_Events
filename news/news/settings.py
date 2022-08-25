cdf95ed7d24af12db9bdae796783c8e178ff70ce
Andriinef <andrii.nefedov@nure.ua>
2022-08-25 12:20:50 +0300

    style

--- news/news/settings.py
+++ news/news/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^(1r&-i_$ggm*ok0kmy&+g-ye(li$jr1x9cnsamef$$gl(#5eu"
 
 # SECURITY WARNING: don"t run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", '.herokuapp.com']
 
@@ -130,10 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "news")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "news/static"),
-    os.path.join(BASE_DIR, "events/static"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 # Default primary key field type
